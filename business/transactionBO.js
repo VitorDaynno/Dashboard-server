8c85c7c5bac7e784b3b5d69fa8cc01132c4f7ced
vitorDaynno <vitordaynno@gmail.com>
2018-01-14 03:42:13 -0200

    Rota funcionando

--- business/transactionBO.js
+++ business/transactionBO.js
@@ -8,7 +8,7 @@ module.exports = function() {
             return new Promise(function (resolve,reject){
                 logger.log('info','[bussines-transactionBO] Getting transaction from database');
                 filter = {};
-                transaction.find(filter).exec()
+                transaction.find(filter).sort({'date': -1}).limit(30).exec()
                     .then(function (transactions){
                         logger.log('info','[business-transactionBO] %d transaction were returned',transactions.length);
                         resolve(transactions);
